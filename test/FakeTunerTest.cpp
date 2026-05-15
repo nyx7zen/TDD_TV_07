382a535f72c052c57bf56f37d162c2cd0416a2db
nyx7zen <nyx7zen@gmail.com>
2026-05-15 14:20:54 +0900

    test: FakeTuner setCH 경계값 테스트 추가

--- test/FakeTunerTest.cpp
+++ test/FakeTunerTest.cpp
@@ -1,7 +1,22 @@
 #include <gtest/gtest.h>
 #include "FakeTuner.h"
 
-TEST(FakeTunerTest, Stub) {
+// S: setCH 최솟값 경계
+TEST(FakeTunerTest, SetCH_Min_ReturnsZero) {
+    // Given
     FakeTuner tuner({1, 4, 12, 56});
+    // When
+    tuner.setCH("0");
+    // Then
     EXPECT_EQ("0", tuner.getCurrentCH());
+}
+
+// S: setCH 최댓값 경계
+TEST(FakeTunerTest, SetCH_Max_Returns99) {
+    // Given
+    FakeTuner tuner({1, 4, 12, 56});
+    // When
+    tuner.setCH("99");
+    // Then
+    EXPECT_EQ("99", tuner.getCurrentCH());
 }
