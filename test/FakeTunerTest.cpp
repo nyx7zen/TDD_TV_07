b326304f38ec3fa8bcfea05c74c85e6f612cea04
nyx7zen <nyx7zen@gmail.com>
2026-05-15 15:18:24 +0900

    refactor: FakeTunerTest Given-When-Then 주석 일관성 정리

--- test/FakeTunerTest.cpp
+++ test/FakeTunerTest.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include "FakeTuner.h"
 
-// S: setCH 최솟값 경계
 TEST(FakeTunerTest, SetCH_Min_ReturnsZero) {
     // Given
     FakeTuner tuner({1, 4, 12, 56});
@@ -11,7 +10,6 @@ TEST(FakeTunerTest, SetCH_Min_ReturnsZero) {
     EXPECT_EQ("0", tuner.getCurrentCH());
 }
 
-// S: setCH 최댓값 경계
 TEST(FakeTunerTest, SetCH_Max_Returns99) {
     // Given
     FakeTuner tuner({1, 4, 12, 56});
@@ -21,7 +19,6 @@ TEST(FakeTunerTest, SetCH_Max_Returns99) {
     EXPECT_EQ("99", tuner.getCurrentCH());
 }
 
-// setCH 범위 초과 예외
 TEST(FakeTunerTest, SetCH_Negative_ThrowsException) {
     // Given
     FakeTuner tuner({1, 4, 12, 56});
@@ -36,7 +33,6 @@ TEST(FakeTunerTest, SetCH_Over99_ThrowsException) {
     EXPECT_THROW(tuner.setCH("100"), std::invalid_argument);
 }
 
-// seekCH wrap-around
 TEST(FakeTunerTest, SeekCH_WrapAround_ReturnsFirst) {
     // Given
     FakeTuner tuner({1, 4, 12, 56});
@@ -47,7 +43,6 @@ TEST(FakeTunerTest, SeekCH_WrapAround_ReturnsFirst) {
     EXPECT_EQ("1", result);
 }
 
-// seekCH 연속 호출
 TEST(FakeTunerTest, SeekCH_Sequential_ReturnsValidChannel) {
     // Given
     FakeTuner tuner({1, 4, 12, 56});
